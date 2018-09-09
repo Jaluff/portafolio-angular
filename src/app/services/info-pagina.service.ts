dea5dd950bee1d9dc2b3f8657adfe5ba82159323
Emilio Jaluff <jaluff@gmail.com>
2018-09-09 20:24:14 -0300

    final clase 34

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,34 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = true;
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
     console.log('servicio de info pagina');
+    this.cargarInfo();
+    this.cargarEquipo();
 
-    // leer json
+   }
 
-    this.http.get('assets/data/data-pagina.json')
+   private cargarInfo() {
+      // leer json
+      this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
+        // console.log(resp);
+      });
+   }
+
+   private cargarEquipo() {
+      // leer json
+      this.http.get('https://angular-udemy-800ee.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[])  => {
+
+        this.equipo = resp;
         console.log(resp);
       });
+
    }
 }
